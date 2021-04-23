ac1ee2fbec5ad5cb823622dba359c6c470584b9b
Ydi21 <77703821+Ydi21@users.noreply.github.com>
2021-04-23 14:48:59 -0400

    Add files via upload

---
+++ dbot.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands, tasks
+from itertools import cycle
+
+# prefix
+client = commands.Bot(command_prefix='.') 
+
+status = cycle(['T', 'Tr', 'Tru', 'True', 'True!'])
+
+# starts status loop
+@client.event 
+async def on_ready():
+    change_status.start() 
+    print('Bot rdy')
+
+# status loop
+@tasks.loop(seconds=5) 
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
+
+# ping command
+@client.command() 
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+# true ping
+@client.event 
+async def on_message(message):
+    if client.user.mentioned_in(message):
+        await message.channel.send('{}'.format(message.author.mention) + ", True!" )
+
+# bot token
+client.run("ODM1MTYwNDIxMTc4NTQwMTEy.YILZwA.Fk7DB2TVkKbKF0fcQBUxvAd5yjs")
